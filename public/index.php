f671fe4687af79d5ef2f14e61c2e300caff5b831
Russ Taylor <russt@etsy.com>
2017-08-07 21:37:13 +0000

    Made it into a _very_ simple Slim app.

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require '../bootstrap.php';
+
+$logFile = '../log/test.log';
+
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+
+$app = new \Slim\App($config);
+
+$app->get('/', function($request, $response, $args) {
+    return $response->write("Hello, you!");
+});
+
+$app->run();
